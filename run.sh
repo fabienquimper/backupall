0ec3a6532fd1afc962e4f9225f6e5f61aa3fc560
Votre Nom <Vous@exemple.comgit config --global user.name Votre>
2019-11-04 14:15:33 +0100

    Add network docker0 for the bridge

--- run.sh
+++ run.sh
@@ -3,7 +3,9 @@
 
 # Run expose server
 #HOSTIP=`ip -4 addr show scope global dev eth0 | grep inet | awk '{print \$2}' | cut -d / -f 1`
-HOSTIP=`ifconfig wlo1 | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`
+#HOSTIP=`ifconfig wlo1 | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`
+HOSTIP=`ifconfig docker0 | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`
+#HOSTIP=172.17.0.1
 echo "Your IP is: ${HOSTIP}"
 sudo docker run --add-host=docker:$HOSTIP --rm -it --name running-backup-server -p 80:7000 -p 8000:8000 -p 8001:8001 -p 8002:8002 -p 8003:8003 backup-server
 echo "Server is running on http://localhost:80"
